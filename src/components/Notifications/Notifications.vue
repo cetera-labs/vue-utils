8cf881922c601531af04c588818c4134151a4e39
Roman Romanov <nicodim@mail.ru>
2026-04-16 20:58:00 +0500

    feat: add Notifications component

---
+++ src/components/Notifications/Notifications.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="fixed bottom-4 right-4 z-50 flex flex-col gap-2">
+        <TransitionGroup
+            enter-active-class="transition duration-300 ease-out"
+            enter-from-class="opacity-0 translate-y-2"
+            enter-to-class="opacity-100 translate-y-0"
+            leave-active-class="transition duration-200 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+            <div
+                v-for="n in notifications"
+                :key="n.id"
+                class="rounded-lg px-4 py-3 text-sm text-white shadow-lg"
+                :class="{
+                    'bg-green-600': n.type === 'success',
+                    'bg-blue-500': n.type === 'info',
+                    'bg-yellow-500': n.type === 'warn',
+                    'bg-red-600': n.type === 'error',
+                }"
+            >
+                {{ n.message }}
+            </div>
+        </TransitionGroup>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useNotify } from '../../composables/useNotify'
+
+const { notifications } = useNotify()
+</script>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,2 @@
-// Export components here
-// Example: export { default as MyComponent } from './MyComponent.vue'
-
-export {}
+export { default as Notifications } from './Notifications/Notifications.vue'
 
